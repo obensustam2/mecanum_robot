85b656532c2f555168904359f61c57ae04b026c6
obensustam2 <obensustam2@gmail.com>
2023-06-05 22:17:49 +0200

    navigation update

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ## Installations
 ```bash
-sudo apt-get install ros-<ROS_VERSION>-teleop-twist-keyboard 
+sudo apt-get install ros-<ROS_VERSION>-teleop-twist-keyboard
+sudo apt-get install ros-<ROS_VERSION>-gmapping
+sudo apt-get install ros-<ROS_VERSION>-amcl 
+sudo apt-get install ros-<ROS_VERSION>-navigation 
 ```
 
 ## Teleop Control
@@ -9,8 +12,24 @@ roslaunch mecanum_robot world.launch
 rosrun teleop_twist_keyboard teleop_twist_keyboard.py
 ```
 
+## Mapping 
+```bash
+roslaunch mecanum_robot world.launch
+roslaunch mecanum_robot gmapping.launch
+rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+rosrun map_server map_saver -f ~/<folder_name>/<map_name>
+```
+
 ## Autonomous Navigation
 ```bash
 roslaunch mecanum_robot world.launch
 roslaunch mecanum_robot navigation.launch
+```
+
+## Mechanical Properties
+```bash
+Wheel Width: 31.5 mm
+Wheel Radius: ~ 30 mm
+Wheel Base: 235 mm
+Track: ~ 270mm
 ```
---
+++ scripts/move_goal.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import rospy
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import Pose, Point, Quaternion
+
+def send_goal():
+    # Initialize the ROS node
+    rospy.init_node('send_goal_node')
+
+    # Create a SimpleActionClient for the move_base action
+    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+    rospy.loginfo("Waiting for move_base action server...")
+
+    # Wait for the action server to start
+    client.wait_for_server()
+
+    rospy.loginfo("Connected to move_base action server")
+
+    # Create a goal position and orientation
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+
+    # Set the goal position
+    goal.target_pose.pose.position = Point(1.0, 2.0, 0.0)
+
+    # Set the goal orientation (as a quaternion)
+    goal.target_pose.pose.orientation = Quaternion(0.0, 0.0, 0.0, 1.0)
+
+    # Send the goal to the action server
+    rospy.loginfo("Sending goal...")
+    client.send_goal(goal)
+
+    # Wait for the action to complete or fail
+    client.wait_for_result()
+
+    # Get the result of the action
+    result = client.get_result()
+
+    if result:
+        rospy.loginfo("Goal reached!")
+    else:
+        rospy.loginfo("Failed to reach the goal.")
+
+if __name__ == '__main__':
+    try:
+        send_goal()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Interrupted by user")
